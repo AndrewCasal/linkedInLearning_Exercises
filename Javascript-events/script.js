890d1429d3b36e42258b3023519225a287e7f86d
andrew <casal.andrew124@gmail.com>
2022-09-27 15:28:07 -0400

    created event listener

--- Javascript-events/script.js
+++ Javascript-events/script.js
@@ -2,4 +2,6 @@ var dragndrop = (function() {
     var myX = '';
     var myY = '';
     var whichArt = '';
+
+    document.querySelector('body').addEventListener('dragstart', moveStart, false);
 })();
