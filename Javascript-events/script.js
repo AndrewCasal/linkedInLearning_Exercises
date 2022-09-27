1a877d69927325a5fc890062949843cf456fdf50
andrew <casal.andrew124@gmail.com>
2022-09-27 15:31:48 -0400

    updated zIndex when event happens

--- Javascript-events/script.js
+++ Javascript-events/script.js
@@ -7,6 +7,7 @@ var dragndrop = (function() {
         whichArt = e.target;
         myX = e.offsetX === undefined ? e.layerX : e.offsetX;
         myY = e.offsetY === undefined ? e.layerY : e.offetY;
+        whichArt.style.zIndex = 10;
     }
 
     document.querySelector('body').addEventListener('dragstart', moveStart, false);
