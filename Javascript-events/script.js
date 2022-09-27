af363388c1ec616884fc5d8f8c3c446edc13ef7e
andrew <casal.andrew124@gmail.com>
2022-09-27 15:30:21 -0400

    created moveStart & definied whichArt, myX

--- Javascript-events/script.js
+++ Javascript-events/script.js
@@ -3,5 +3,11 @@ var dragndrop = (function() {
     var myY = '';
     var whichArt = '';
 
+    function moveStart(e) {
+        whichArt = e.target;
+        myX = e.offsetX === undefined ? e.layerX : e.offsetX;
+        myY = e.offsetY
+    }
+
     document.querySelector('body').addEventListener('dragstart', moveStart, false);
 })();
