c013626ae0d5a083ff21295e43da2064a514f24f
andrew <casal.andrew124@gmail.com>
2022-09-27 15:31:19 -0400

    defined myY

--- Javascript-events/script.js
+++ Javascript-events/script.js
@@ -6,7 +6,7 @@ var dragndrop = (function() {
     function moveStart(e) {
         whichArt = e.target;
         myX = e.offsetX === undefined ? e.layerX : e.offsetX;
-        myY = e.offsetY
+        myY = e.offsetY === undefined ? e.layerY : e.offetY;
     }
 
     document.querySelector('body').addEventListener('dragstart', moveStart, false);
